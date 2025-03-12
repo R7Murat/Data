f7ceb50758e8277dba49f882be58310e474f674f
R7Murat <arseven.murat@gmail.com>
2025-03-12 15:20:31 +0300

    Update settings.py
    update

--- BlogDjango/src/cblog/settings.py
+++ BlogDjango/src/cblog/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': 'blog', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
         'PASSWORD': config('PASSWORD'),
-        'HOST': 'awsproject-rds.cra4ioeq8ymy.us-east-1.rds.amazonaws.com',  # database endpoint is written here
+        'HOST': 'aws-final-proje.cra4ioeq8ymy.us-east-1.rds.amazonaws.com',  # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
 }
